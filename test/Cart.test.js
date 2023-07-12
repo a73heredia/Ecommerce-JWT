9d1b84c916fd2aee77238f6e8270ffa4d0d6e0c9
a73heredia <cristianalejandroheredia@gmail.com>
2023-07-11 21:54:59 -0300

    tests

---
+++ test/Cart.test.js
@@ -0,0 +1,54 @@
+import chai from 'chai';
+import mongoose from 'mongoose';
+import Cart from '../dao/cart.js';
+import Product from '../dao/product.js'
+const expect = chai.expect;
+
+mongoose.connect('mongodb+srv://cristianalejandroheredia:catalina87@cluster0.qtpeto6.mongodb.net/ecommerce-test?retryWrites=true&w=majority');
+
+describe('Pruebas al modulo Cart dao', function () {
+    before(function() {
+        console.log('Pruebas de Carritos');
+    });
+
+    
+    beforeEach(async function() {
+        mongoose.connection.collections.carts.drop();
+   });
+
+   after(() => {
+});
+
+afterEach(() => {});
+
+it('Debe crear un carrito de forma exitosa', async function() {
+    const cart = await Cart.createCart({products: []})
+
+    expect(cart).to.be.have.property('_id');
+    expect(cart).to.be.have.property('products');
+    expect(Array.isArray(cart.products)).to.be.ok;
+    expect(cart.products).to.be.deep.equal([]);
+    
+    
+});
+
+it('Debe obtener los carritos', async function() {
+    const carts = await Cart.getCarts();
+    // expect(carts).to.be.equal([])
+    // expect(carts).to.have.lengthOf(0)
+    expect(Array.isArray(carts)).to.be.equal(true)
+    expect(Array.isArray(carts)).to.be.ok
+});
+
+it('Borrar un carrito de forma exitosa', async function() {
+    const cart = await Cart.createCart({products: []})
+
+    await Cart.deleteCartById(cart._id)
+    
+    
+    expect(cart.products).to.have.lengthOf(0)
+});
+
+
+
+});
---
+++ test/Product.test.js
@@ -0,0 +1,109 @@
+import mongoose from 'mongoose';
+import Product from '../dao/product.js';
+import Assert from 'assert';
+
+const assert = Assert.strict;
+
+const connection = mongoose.connect('mongodb+srv://cristianalejandroheredia:catalina87@cluster0.qtpeto6.mongodb.net/ecommerce-test?retryWrites=true&w=majority');
+
+describe('Pruebas al modulo de productos dao', function() {
+    before(async function() {
+        console.log('Pruebas Productos');
+    });
+
+    beforeEach(async function() {
+         mongoose.connection.collections.products.drop();
+    });
+
+    after('[after]', function() {
+        console.log('Despues de todas las pruebas');
+    });
+    
+    afterEach('[afterEach]',function(){
+        console.log('Despues de cada prueba');
+    });
+
+    // it('debe traer los productos', async function() {
+    //     const result = await Product.getProducts();
+    //     console.log(result);
+    // });
+
+    it('Debe crear un producto', async function() {
+        let result = await Product.createProduct({
+            name: 'ale',
+            price: 123,
+            code: 'abc123',
+            description: 'alejafdsf',
+            stock: 4,
+            status: 'available',
+            category: 'ropa'
+        });
+        assert.ok(result._id);
+        assert.strictEqual(result.name, 'ale')
+    });
+
+    it('debe retornar todos los productos', async function() {
+        const result = await Product.getProducts();
+
+        assert.strictEqual(Array.isArray(result), true);
+        assert.deepStrictEqual(result, []);
+
+    });
+
+    it('Debe retornar todos los productos', async function() {
+        const result = await Product.createProduct({
+            name: 'ale',
+            price: 123,
+            code: 'abc123',
+            description: 'alejafdsf',
+            stock: 4,
+            status: 'available',
+            category: 'ropa'
+        });
+        const product = await Product.getProductById(result._id);
+        //console.log(product);
+        assert.strictEqual(typeof product, 'object');
+        
+    });
+
+    it('Debe eliminar un producto por id', async function() {
+        const result = await Product.createProduct({
+            name: 'ale',
+            price: 123,
+            code: 'abc123',
+            description: 'alejafdsf',
+            stock: 4,
+            status: 'available',
+            category: 'ropa'
+        });
+         await Product.deleteProductById(result._id);
+        
+         //console.log(product);        
+    });
+
+    it('debe actualizar un producto por id', async function() {
+        const result = await Product.createProduct({
+            name: 'ale1',
+            price: 121,
+            code: 'abc121',
+            description: 'alejafdsf1',
+            stock: 41,
+            status: 'available1',
+            category: 'ropa1'
+        });
+
+        const data = {
+            name: 'nuevo Producto',
+            price: 22
+        }
+
+        await Product.updateProductById(result._id, data);
+
+        const product = await  Product.getProductById(result._id);
+        console.log(product);
+         assert.ok(product._id);
+         assert.strictEqual(typeof product, 'object');
+         assert.strictEqual(product.name, data.name);
+        
+    });
+} );
