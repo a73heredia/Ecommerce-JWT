50d58e06cc8f27957de1276fcd61f3b5ea975f96
a73heredia <cristianalejandroheredia@gmail.com>
2023-08-05 16:31:09 -0300

    deploy

--- db/mongodb.js
+++ db/mongodb.js
@@ -5,7 +5,9 @@ dotenv.config();
 export const init = async () =>  {
     try {
         const URI = process.env.MONGO_URI;
-        await mongoose.connect(URI);
+        await mongoose
+            .set('strictQuery', true)
+            .connect(URI);
         console.log('Database Connected');
     } catch (error) {
         console.log(error.message);
