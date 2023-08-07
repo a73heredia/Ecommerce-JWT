a00f602d893251bb4c9f2428e6f3a9bfdde2145f
a73heredia <cristianalejandroheredia@gmail.com>
2023-08-07 09:47:21 -0300

    dao mensajes

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,25 +1,25 @@
 import MessageModel from '../models/message.js'
+import Messages from '../dao/messages.js'
 import { emit } from '../socket.js'
 
 class MessageController {
-    //Crear Mensaje
-    static async create(req, res) {
-        const { body } = req
-        const message = await MessageModel.create(body)
-        emit(message)
-        res.status(201).json({mensaje: message})
-    }
-
-    //Traer todos los mensajes
-    static async get(req, res) {
-        const result = await MessageModel.find()
-        res.status(200).json(result)
-    }
-
-    //BUSCO MENSAJES POR ID
+ //CREO MENSAJES
+static async create(req, res) {
+  const { body } = req
+  console.log(body)
+  const mensaje = await Messages.createMessage(body)
+  emit(mensaje)
+  res.status(201).json(mensaje)
+}
+//BUSCO TODOS LOS MENSAJES
+  static async get(req, res) {
+    const result = await Messages.getMessages()
+    res.status(200).json(result)
+  }
+//BUSCO MENSAJES POR ID
   static async getById(req, res) {
     const { params: { id } } = req
-    const result = await MessageModel.findById(id)
+    const result = await Messages.getMessageById(id)
     if (!result) {
       return res.status(404).end()
     }
@@ -27,16 +27,19 @@ class MessageController {
   }
 //MODIFICO MENSAJES POR ID
   static async updateById(req, res) {
-    const { params: { id }, body } = req
-    await MessageModel.updateOne({ _id: id }, { $set: body })
+    //const { params: { id }, body } = req
+    const id = req.params.id
+    const data = req.body
+    await Messages.updateMById(id, data)
     res.status(204).end()
   }
 //ELIMINO MENSAJES POR ID
   static async deleteById(req, res) {
     const { params: { id } } = req
-    await MessageModel.deleteOne({ _id: id })
+    await Messages.deleteM({ _id: id })
     res.status(204).end()
   }
+
 
 }
 
